0362adb83f79dd04b1a5e32bfd4b8b777faacd7c
Christian Wang <christianwang39@gmail.com>
2025-10-26 00:41:20 -0700

    fixed bug

--- .next 2/server 2/app/api/users/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/users/route.js")
-R.c("server/chunks/[root-of-the-server]__03fe02e0._.js")
-R.c("server/chunks/[root-of-the-server]__f32ddc7b._.js")
-R.c("server/chunks/_next-internal_server_app_api_users_route_actions_4b9121e3.js")
-R.m(24452)
-module.exports=R.m(24452).exports
